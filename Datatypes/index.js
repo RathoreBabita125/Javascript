164d029ac37d9e5dbd8a392dbc152df658e20758
Babita <144489058+RathoreBabita125@users.noreply.github.com>
2025-04-29 19:04:35 +0000

    datatypes

---
+++ Datatypes/index.js
@@ -0,0 +1,31 @@
+// datatypes
+"use strict"  // treat all js code to new version
+
+// alert("hello"); //we are using node js, not browesr
+
+
+//types to datatype
+//**************primitive datatype************** */
+// Number -  2 to power 53
+// String - ""
+// Boolean - true/false
+// null - standalone value
+// undefined
+// BigInt
+// Symbol-> Unique
+
+let name="John"
+let age=56
+let isdefined=false
+let val=null
+let x;
+
+// console.table([name,age,isdefined,val,x])
+
+
+//*********************OBJECT************** */
+console.log(typeof(name))  //string
+console.log(typeof(age))    //number
+console.log(typeof(isdefined))  //boolean
+console.log(typeof(val))  //object
+console.log(typeof(x))   //undefined
