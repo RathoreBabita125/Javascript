4894b47a34e4d4a7f6af1c2264d0f42de20932cd
Babita <144489058+RathoreBabita125@users.noreply.github.com>
2025-04-29 18:02:53 +0000

    variable

---
+++ Variable/index.js
@@ -0,0 +1,34 @@
+  // different types of variables
+
+
+//   1. const varaible  : const variable are the constant variables that can't be changed after intialization
+const id=56566
+// id=154987   //TypeError: Assignment to constant variable.
+
+// 2. let variable: let variable are the variables that can be changed after intialization
+// let name="John"
+
+// we can also declare only variable without intialization
+let name;
+
+//3. var variable: var variable are the variables that can be changed after intialization
+
+    /*the var keyword is used to declare variables that are function-scoped or globally-scoped,
+     depending on where they are declared. Variables declared with var are hoisted to the top 
+     of their containing function or script, which means they can be used before they are declared
+     in the code,though they will be initialized to undefined */
+
+
+
+//4. simple intialization to variable
+
+email="abc@gmail.com"
+
+
+// console.log(id);    
+// console.log(name);
+// console.log(email);
+
+console.table([id,name,email])
+
+
