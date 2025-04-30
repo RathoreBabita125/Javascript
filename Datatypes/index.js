759357d5ea599ced29f9407c126786d86062e63f
Babita <144489058+RathoreBabita125@users.noreply.github.com>
2025-04-30 12:01:14 +0000

    datatypes

--- Datatypes/index.js
+++ Datatypes/index.js
@@ -24,8 +24,68 @@ let x;
 
 
 //*********************OBJECT************** */
-console.log(typeof(name))  //string
-console.log(typeof(age))    //number
-console.log(typeof(isdefined))  //boolean
-console.log(typeof(val))  //object
-console.log(typeof(x))   //undefined
+// console.log(typeof(name))  //string
+// console.log(typeof(age))    //number
+// console.log(typeof(isdefined))  //boolean
+// console.log(typeof(val))  //object
+// console.log(typeof(x))   //undefined
+
+
+// ************There are two types of datatypes******************
+// ******************Primitive Datatypes (There are 7 primitive datatypes)
+// String 
+// Number
+// BigInt
+// Boolean
+// null
+// undefined
+// Symbol
+
+let id=1005
+let anotherId=110.2
+
+let str;  //undefined
+
+// console.log(typeof id)
+// console.log(typeof anotherId);
+
+let bigNumber=458965329898455198776974145654n
+// console.log(typeof bigNumber);
+
+let isprooved=false
+// console.log(isprooved);
+
+let num='123'
+let anotherNum='123'
+// console.log(Symbol(num)===Symbol(anotherId));  //false
+// console.log(num);
+// console.log(anotherNum);
+
+// console.log(num==anotherNum);    //true
+// console.log(typeof num);
+// console.log(anotherNum);
+
+
+// ****************Not Primitive Datatype OR Reference 
+// Array
+// object
+// function
+
+let team=["john", "mike","harvay"]   //Array
+console.log(typeof team);
+
+
+//object
+let studentDetails={
+    name:"John",
+    id:12345,
+    section:'A'
+}
+console.log(typeof studentDetails);
+
+//function as variable
+const fun=function(){
+    console.log("Hey, there where are you going");   
+}
+console.log(typeof fun);
+
