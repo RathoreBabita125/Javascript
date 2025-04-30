6a205bf26b295bcd76f8405fe4bdcb4b4b7583e3
Babita <144489058+RathoreBabita125@users.noreply.github.com>
2025-04-30 10:11:14 +0000

    operations

---
+++ Operations/index.js
@@ -0,0 +1,50 @@
+//**********************OPERATIONS*******************/
+
+let val=10
+// console.log(-val);
+
+// console.log(val+2);
+// console.log(val-2);
+// console.log(val*2);
+// console.log(val/2);         
+// console.log(val**2);
+// console.log(val%2);
+
+let str1="Hello"
+let str2=" John"
+let str3=str1+str2
+// console.log(str3);
+
+let x=10
+let y="10"
+// console.log(x+y)
+
+let a="10"
+let b=10
+// console.log(a+b);
+
+// console.log(10+"2");
+// console.log("10"+2);
+// console.log("10"+2+5);
+// console.log(10+2+"5");
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+);//  syntax error
+
+// console.log("");
+// console.log(+"");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
