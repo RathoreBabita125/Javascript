5b8cdb788ca5048c98aa09d84ee1290c61c2d3d6
Babita <144489058+RathoreBabita125@users.noreply.github.com>
2025-04-30 09:30:50 +0000

    conversion datatype

---
+++ Datatypes_Conversions/index.js
@@ -0,0 +1,40 @@
+// *************Datatypes conversion***************************
+// let num=100
+// console.log(num)
+
+// let stringNumber=String(num)
+// console.log(typeof stringNumber);
+
+// let num="100"
+// console.log(typeof num);
+
+// let digit="30asb"
+// let digitNumber=Number(digit)
+
+// console.log(typeof digit);
+// console.log(typeof digitNumber);
+
+// console.log(digitNumber);  // NaN ====>   Not a Number
+
+
+
+// let isdefined=""
+// let isBool=isdefined
+
+// let isdefined=true
+// let isBool=isdefined
+
+let isdefined=0
+let isBool=Boolean(isdefined)
+
+console.log(typeof isdefined);
+console.log(isBool);
+
+console.log(typeof isBool);
+
+
+
+
+
+
+
