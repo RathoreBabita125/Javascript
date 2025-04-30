f0b966040e051ac9eddf73ff1e5cb4c7a10bbefc
Babita <144489058+RathoreBabita125@users.noreply.github.com>
2025-04-30 10:32:55 +0000

    comparison

---
+++ Comparison/index.js
@@ -0,0 +1,42 @@
+/***********************COMPARISON****************** */
+
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2>1);
+// console.log(2===1);
+
+// console.log(2=="2");
+// console.log(2>="2");
+// console.log(2<"2");
+// console.log("02">1);
+// console.log("2">=2);
+
+
+// console.log(null==0);
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null>=0);  // true
+// console.log(null<=0);  //true
+
+
+// console.log(undefined>0);   //false
+// console.log(undefined<0);   //false 
+// console.log(undefined>=0);   //false
+// console.log(undefined<=0);   //false
+// console.log(undefined==0);   //false
+// console.log(undefined===0);   //false
+
+
+
+//************Strict comparison*************** */
+/*   In Strict Comparison, it will check value with its datatype */
+
+console.log(2=='2');     //true
+console.log(2=="2");     //true
+console.log(2==='2');   //false
+console.log(2==="2");    //false
+
+
+
